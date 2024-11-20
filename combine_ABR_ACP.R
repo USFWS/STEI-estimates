623e8eae679dcf792ca82befea9df7666b03ef91
Erik Osnas <erik_osnas@fws.gov>
2024-11-20 11:58:01 -0900

    updates for combined model

--- combine_ABR_ACP.R
+++ combine_ABR_ACP.R
@@ -121,7 +121,7 @@ summary(fit2)
 ################################################################################
 ## predict and map
 library(mgcv)
-fit <- readRDS(file = "results/fit2.comb.RDS")
+fit <- readRDS(file = "results/fit1.comb.RDS")
 #make map, code from ACP-Mapping/map_GAM.R
 source("../ACP-Mapping/map_density_functions.R")
 acp <- st_read(dsn="../ACP-Mapping/Data/ACP_2023/analysis_output/ACP_DesignStrata_QC.gpkg")
@@ -136,8 +136,8 @@ newdat <- st_centroid(grid) %>%
   mutate(Observer = "HMW", survey = "ABR", logArea = 0, Year = 2013) 
 # %>%
 #   cbind(grid_position)
-preds <- predict(fit, newdata = newdat, type = "response", se.fit = TRUE,
-                 exclude = "s(Observer)", newdata.guaranteed = TRUE)
+preds <- predict(fit, newdata = newdat, type = "response", se.fit = TRUE, 
+                 newdata.guaranteed = TRUE)
 
 plotdat <- cbind(preds, grid) |> st_transform(crs = 4326)
 p <- ggplot(data = plotdat) + geom_sf(aes(fill=fit), col = NA) +
@@ -154,7 +154,7 @@ p2 <- ggplot(data = plotdat) + geom_sf(aes(fill=CV), col = NA) +
 print(p2)
 ggsave("results/combined_map_cv.png")
 min(plotdat$CV)
-#[1] 0.2741714
+#[1] 0.2765243
 #zoom to triangle
 plotdat <- st_transform(plotdat, crs = 4326)
 ggplot(data = plotdat) + geom_sf(aes(fill=fit), col = NA) +
@@ -162,8 +162,17 @@ ggplot(data = plotdat) + geom_sf(aes(fill=fit), col = NA) +
   labs(title = "STEI in 2013") + 
   coord_sf(xlim = c(-157.5, -155.5), ylim = c(70.9, 71.4), expand = FALSE)
 ggsave("results/combined_map_zoomed.png")
+####plot on link scale
+draw(fit, select = 1, rug = FALSE) +
+  theme(axis.text.x=element_blank(), 
+        axis.ticks.x=element_blank(), 
+        axis.text.y=element_blank(), 
+        axis.ticks.y=element_blank())
+ggsave("results/combined_partial_1.png")
+draw(fit, select = 2, rug = FALSE) 
+ggsave("results/combined_partial_2.png")
 #########################################
-# now simulation population total by year
+# now simulated population total by year
 #add Area to newdat
 library(units)
 newdat$Area <- drop_units(set_units(grid$Grid.Area, "km^2"))
@@ -171,62 +180,52 @@ df <- data.frame(NULL)
 for(i in 1999:2024){
   df <- rbind(df, mutate(newdat, Year = i))
 }
-detdf <- data.frame(Bin = 1:4, p = c(0.514, 0.457, 0.143, 0.114), 
-                    lower = c(0.338, 0.217, 0.048, 0.026), 
-                    upper=c(0.689, 0.717, 0.306, 0.310))
-mP <- mean(detdf$p)
-sSE <- sqrt(mean( ((detdf$upper - detdf$lower)/(2*1.96))^2 ))
-#method of moments for Beta distribution
-shape1 = mP*( mP*(1-mP)/sSE^2 - 1)
-shape2 = (1 - mP)*( mP*(1-mP)/sSE^2 - 1)
-Nsamples <- 250
-s <- rbeta(Nsamples, 
-           shape1 = shape1, 
-           shape2 = shape2)
+Nsamples <- 500
 post <- matrix(0, Nsamples, length(unique(df$Year)))
 
 Xp <- predict(fit, type="lpmatrix", newdata=df,
               newdata.guaranteed=TRUE)
 #sample from parameter posterior
-b <- rmvn(Nsamples, coef(fit), vcov(fit))
-for(j in 1:Nsamples ){
-  p <- exp(Xp%*%b[j,]) * as.vector(df$Area) #replicate of prediction at all points
-  post[j,] <- cbind(df, p) %>%
-    group_by(Year) %>%
-    summarize( Total = sum(2*p)) %>% #INDICTED BIRDS!!!
-    ungroup() %>%
-    select(Total) %>%
-    unlist()
-}
-#summarize
-#getting some absurd posterior samples, filter?
-post <- post[apply(post, 1, max) < 10000,]
-sumdf <- data.frame(Year = min(fit$model$Year):max(fit$model$Year),
-                    Mean = apply(post, 2, mean), 
-                    sd = apply(post, 2, sd), 
-                    median = apply(post, 2, median), 
-                    upper = apply(post, 2, quantile, probs = 0.975),
-                    lower = apply(post, 2, quantile, probs = 0.025))
-#plot
-gg <- ggplot(data = sumdf) + 
-  geom_ribbon(aes(x = Year, ymin = lower, ymax = upper), fill = "orange", alpha = 0.5) + 
-  geom_line(aes(x = Year, y = Mean)) + 
-  geom_line(aes(x = Year, y = median), linetype = "dashed") + 
-  scale_x_continuous(breaks = seq(1999, 2025, by = 2)) + 
-  ylab("Indicated Breeding Bird Index")
-print(gg)
+# b <- rmvn(Nsamples, coef(fit), vcov(fit))
+# for(j in 1:Nsamples ){
+#   p <- exp(Xp%*%b[j,]) * as.vector(df$Area) #replicate of prediction at all points
+#   post[j,] <- cbind(df, p) %>%
+#     group_by(Year) %>%
+#     summarize( Total = sum(2*p)) %>% #INDICTED BIRDS!!!
+#     ungroup() %>%
+#     select(Total) %>%
+#     unlist()
+# }
+# #summarize
+# #getting some absurd posterior samples, filter?
+# post <- post[apply(post, 1, max) < 10000,]
+# sumdf <- data.frame(Year = min(fit$model$Year):max(fit$model$Year),
+#                     Mean = apply(post, 2, mean), 
+#                     sd = apply(post, 2, sd), 
+#                     median = apply(post, 2, median), 
+#                     upper = apply(post, 2, quantile, probs = 0.975),
+#                     lower = apply(post, 2, quantile, probs = 0.025))
+# #plot
+# gg <- ggplot(data = sumdf) + 
+#   geom_ribbon(aes(x = Year, ymin = lower, ymax = upper), fill = "orange", alpha = 0.5) + 
+#   geom_line(aes(x = Year, y = Mean)) + 
+#   geom_line(aes(x = Year, y = median), linetype = "dashed") + 
+#   scale_x_continuous(breaks = seq(1999, 2025, by = 2)) + 
+#   ylab("Indicated Breeding Bird Index")
+# print(gg)
 #filter above seems to work OK for fit1 but not fit2
 #wacky, for fit2, direct MN sampling does not work well, large "blowup" of 
 # estimates in first few year. Is this due to posterior sampling or the model?
 #ggsave("results/combined_year_noD_filtered.png")
-##try MH sampling
-b <- gam.mh(fit, ns=2000, thin = 2, rw.scale = 0.02)
+##try MH sampling, need to turn acceptance rate, 0.02 seems to work for fit1
+b <- gam.mh(fit, ns=3000, thin = 2, rw.scale = 0.02)
 b <- b$bs
 plot(b[,1])
 plot(b[,2])
 plot(b[,200])
 hist(b[,1])
-b <- b[sample(1:dim(b)[1], Nsamples),]
+b <- b[sample(750:dim(b)[1], Nsamples),] #just to scramble over any autocorreclation, 
+#  using 750 as start to avoid a long autocorrelated chains before that section
 post <- matrix(0, Nsamples, length(unique(df$Year)))
 for(j in 1:Nsamples ){
   p <- exp(Xp%*%b[j,]) * as.vector(df$Area) #replicate of prediction at all points
@@ -253,92 +252,23 @@ gg <- ggplot(data = sumdf) +
   scale_x_continuous(breaks = seq(1999, 2025, by = 2)) + 
   ylab("Indicated Breeding Bird Index")
 print(gg)
+ggsave("results/combined_year_noD.png")
 #that seems to work for fit1
 #wow, MH didn't work at all for fit2, must be the s-t interact in the model
 #even filter the posterior after the MH step didn't work. Abandone fit2
 ################################################################################
 #add detection
-for(j in 1:Nsamples ){
-  p <- exp(Xp%*%b[j,]) * as.vector(df$Area) #replicate of prediction at all points
-  post[j,] <- cbind(df, p) %>%
-    group_by(Year) %>%
-    summarize( Total = sum(2*p/s[j])) %>% #INDICTED BIRDS!!!
-    ungroup() %>%
-    select(Total) %>%
-    unlist()
-}
-#summarize
-sumdf <- data.frame(Year = min(fit$model$Year):max(fit$model$Year),
-                    Mean = apply(post, 2, mean), 
-                    sd = apply(post, 2, sd), 
-                    median = apply(post, 2, median), 
-                    upper = apply(post, 2, quantile, probs = 0.975),
-                    lower = apply(post, 2, quantile, probs = 0.025))
-#plot
-gg <- ggplot(data = sumdf) + 
-  geom_ribbon(aes(x = Year, ymin = lower, ymax = upper), fill = "orange", alpha = 0.5) + 
-  geom_line(aes(x = Year, y = Mean)) + 
-  scale_x_continuous(breaks = seq(1999, 2025, by = 2)) + 
-  ylab("Estimated Indicated Bird Index") +
-  labs(title = "Estimated breeding bird index across ACP (with detection)")
-print(gg)
-#find long term posterior mean and CI
-m = round(mean(apply(post[,-c(1:5)], 1, mean)), 1)
-q = round(quantile(apply(post[,-c(1:5)], 1, mean), probs=c(0.025, 0.05, 0.5, 0.975)), 1)
-gg + geom_line(aes(x = Year, y = median), linetype = "dashed") + 
-  geom_text(aes(x = 2003, y = 10000, label = "20-year mean", hjust = 0)) + 
-  geom_text(aes(x = 2003, y = 9000, hjust = 0,
-                label = paste0(m, " (95% CI: ", q[1], ", ", q[4], ")")))
-ggsave("results/combined_year_detection.png")
-################################################################################
-#as above but limit spatial extent to ACP "High" and "Tesh High" strata.
-#  above posteriors are very highly skewed to large numbers of eiders
-#  this cause concern with eider experts, so trying to understand cause
-#  Is it a property of the NB model (yes) or due to the spatial predictions into 
-#    the medium and low strata (maybe partly?)
-#  Restrict predictions to the High and Tesh High strata to see effect on posterior
-fit <- readRDS(file = "results/fit.RDS")
-#make map, code from ACP-Mapping/map_GAM.R
-source("../ACP-Mapping/map_density_functions.R")
-acp <- st_read(dsn="../ACP-Mapping/Data/ACP_2023/analysis_output/ACP_DesignStrata_QC.gpkg")
-#make grid
-acp <- select_area(area = acp, select = c(1,5)) %>% #select only "High" and "Tesh High"
-  st_transform(crs=3338)
-plot(st_geometry(acp)) #correct
-grid <- st_intersection(acp, st_make_grid(x=acp, cellsize = 1000)) %>%
-  mutate(Sample.Label = row.names(.), Grid.Area = st_area(.))
-newdat <- st_centroid(grid) %>%
-  st_coordinates() %>%
-  as.data.frame() %>%
-  mutate(Observer = "HMW", logArea = 0, Year = 2013) 
-preds <- predict(fit, newdata = newdat, type = "response", se.fit = TRUE,
-                 exclude = "s(Observer)", newdata.guaranteed = TRUE)
-
-plotdat <- cbind(preds, grid) |> st_transform(crs = 4326)
-p <- ggplot(data = plotdat) + geom_sf(aes(fill=fit), col = NA) +
-  #geom_sf(data = design[-trim,], fill = NA, alpha = 1, col = "white")  +
-  scale_fill_viridis_c(name = "Expected \n density") +
-  #breaks = my_breaks, labels = my_breaks) +
-  labs(title = "STEI in 2013")
-print(p)
-ggsave("results/combined_map_High.png")
-#find posterior by year, here just with detection
-library(units)
-newdat$Area <- drop_units(set_units(grid$Grid.Area, "km^2"))
-df <- data.frame(NULL)
-for(i in 1999:2023){
-  df <- rbind(df, mutate(newdat, Year = i))
-}
-Nsamples <- 250
+detdf <- data.frame(Bin = 1:4, p = c(0.514, 0.457, 0.143, 0.114), 
+                    lower = c(0.338, 0.217, 0.048, 0.026), 
+                    upper=c(0.689, 0.717, 0.306, 0.310))
+mP <- mean(detdf$p)
+sSE <- sqrt(mean( ((detdf$upper - detdf$lower)/(2*1.96))^2 ))
+#method of moments for Beta distribution
+shape1 = mP*( mP*(1-mP)/sSE^2 - 1)
+shape2 = (1 - mP)*( mP*(1-mP)/sSE^2 - 1)
 s <- rbeta(Nsamples, 
-           shape1 = 0.3*( 0.3*(1-0.3)/0.0555^2 - 1), 
-           shape2 = (1 - 0.3)*( 0.3*(1-0.3)/0.0555^2 - 1))
-post <- matrix(0, Nsamples, length(unique(df$Year)))
-
-Xp <- predict(fit, type="lpmatrix", newdata=df, exclude = "s(Observer)",
-              newdata.guaranteed=TRUE)
-#sample from parameter posterior
-b <- rmvn(Nsamples, coef(fit), vcov(fit))
+           shape1 = shape1, 
+           shape2 = shape2)
 for(j in 1:Nsamples ){
   p <- exp(Xp%*%b[j,]) * as.vector(df$Area) #replicate of prediction at all points
   post[j,] <- cbind(df, p) %>%
@@ -347,7 +277,6 @@ for(j in 1:Nsamples ){
     ungroup() %>%
     select(Total) %>%
     unlist()
-  print(j)
 }
 #summarize
 sumdf <- data.frame(Year = min(fit$model$Year):max(fit$model$Year),
@@ -360,15 +289,94 @@ sumdf <- data.frame(Year = min(fit$model$Year):max(fit$model$Year),
 gg <- ggplot(data = sumdf) + 
   geom_ribbon(aes(x = Year, ymin = lower, ymax = upper), fill = "orange", alpha = 0.5) + 
   geom_line(aes(x = Year, y = Mean)) + 
+  geom_line(aes(x = Year, y = median), linetype = "dashed") + 
   scale_x_continuous(breaks = seq(1999, 2025, by = 2)) + 
-  ylab("Estimated Indicated Bird Index") +
-  labs(title = "Estimated breeding bird index High and Tesh High Strata (with detection)")
+  ylab("Indicated Breeding Bird")
 print(gg)
+ggsave("results/combined_year_withD.png")
+saveRDS(post, file = "results/Comb_post.RDS")
 #find long term posterior mean and CI
-m = round(mean(apply(post[,-c(1:5)], 1, mean)), 1)
-q = round(quantile(apply(post[,-c(1:5)], 1, mean), probs=c(0.025, 0.05, 0.5, 0.975)), 1)
-gg + geom_line(aes(x = Year, y = median), linetype = "dashed") + 
-  geom_text(aes(x = 2003, y = 3000, label = "20-year mean", hjust = 0)) + 
-  geom_text(aes(x = 2003, y = 2800, hjust = 0,
-                label = paste0(m, " (95% CI: ", q[1], ", ", q[4], ")")))
-ggsave("results/combined_year_detection_High.png")
+# q = round(quantile(apply(post[,-c(1:5)], 1, mean), probs=c(0.025, 0.05, 0.5, 0.975)), 1)
+
+# ################################################################################
+# #as above but limit spatial extent to ACP "High" and "Tesh High" strata.
+# #  above posteriors are very highly skewed to large numbers of eiders
+# #  this cause concern with eider experts, so trying to understand cause
+# #  Is it a property of the NB model (yes) or due to the spatial predictions into 
+# #    the medium and low strata (maybe partly?)
+# #  Restrict predictions to the High and Tesh High strata to see effect on posterior
+# fit <- readRDS(file = "results/fit.RDS")
+# #make map, code from ACP-Mapping/map_GAM.R
+# source("../ACP-Mapping/map_density_functions.R")
+# acp <- st_read(dsn="../ACP-Mapping/Data/ACP_2023/analysis_output/ACP_DesignStrata_QC.gpkg")
+# #make grid
+# acp <- select_area(area = acp, select = c(1,5)) %>% #select only "High" and "Tesh High"
+#   st_transform(crs=3338)
+# plot(st_geometry(acp)) #correct
+# grid <- st_intersection(acp, st_make_grid(x=acp, cellsize = 1000)) %>%
+#   mutate(Sample.Label = row.names(.), Grid.Area = st_area(.))
+# newdat <- st_centroid(grid) %>%
+#   st_coordinates() %>%
+#   as.data.frame() %>%
+#   mutate(Observer = "HMW", logArea = 0, Year = 2013) 
+# preds <- predict(fit, newdata = newdat, type = "response", se.fit = TRUE,
+#                  exclude = "s(Observer)", newdata.guaranteed = TRUE)
+# 
+# plotdat <- cbind(preds, grid) |> st_transform(crs = 4326)
+# p <- ggplot(data = plotdat) + geom_sf(aes(fill=fit), col = NA) +
+#   #geom_sf(data = design[-trim,], fill = NA, alpha = 1, col = "white")  +
+#   scale_fill_viridis_c(name = "Expected \n density") +
+#   #breaks = my_breaks, labels = my_breaks) +
+#   labs(title = "STEI in 2013")
+# print(p)
+# ggsave("results/combined_map_High.png")
+# #find posterior by year, here just with detection
+# library(units)
+# newdat$Area <- drop_units(set_units(grid$Grid.Area, "km^2"))
+# df <- data.frame(NULL)
+# for(i in 1999:2023){
+#   df <- rbind(df, mutate(newdat, Year = i))
+# }
+# Nsamples <- 250
+# s <- rbeta(Nsamples, 
+#            shape1 = 0.3*( 0.3*(1-0.3)/0.0555^2 - 1), 
+#            shape2 = (1 - 0.3)*( 0.3*(1-0.3)/0.0555^2 - 1))
+# post <- matrix(0, Nsamples, length(unique(df$Year)))
+# 
+# Xp <- predict(fit, type="lpmatrix", newdata=df, exclude = "s(Observer)",
+#               newdata.guaranteed=TRUE)
+# #sample from parameter posterior
+# b <- rmvn(Nsamples, coef(fit), vcov(fit))
+# for(j in 1:Nsamples ){
+#   p <- exp(Xp%*%b[j,]) * as.vector(df$Area) #replicate of prediction at all points
+#   post[j,] <- cbind(df, p) %>%
+#     group_by(Year) %>%
+#     summarize( Total = sum(2*p/s[j])) %>% #INDICTED BIRDS!!!
+#     ungroup() %>%
+#     select(Total) %>%
+#     unlist()
+#   print(j)
+# }
+# #summarize
+# sumdf <- data.frame(Year = min(fit$model$Year):max(fit$model$Year),
+#                     Mean = apply(post, 2, mean), 
+#                     sd = apply(post, 2, sd), 
+#                     median = apply(post, 2, median), 
+#                     upper = apply(post, 2, quantile, probs = 0.975),
+#                     lower = apply(post, 2, quantile, probs = 0.025))
+# #plot
+# gg <- ggplot(data = sumdf) + 
+#   geom_ribbon(aes(x = Year, ymin = lower, ymax = upper), fill = "orange", alpha = 0.5) + 
+#   geom_line(aes(x = Year, y = Mean)) + 
+#   scale_x_continuous(breaks = seq(1999, 2025, by = 2)) + 
+#   ylab("Estimated Indicated Bird Index") +
+#   labs(title = "Estimated breeding bird index High and Tesh High Strata (with detection)")
+# print(gg)
+# #find long term posterior mean and CI
+# m = round(mean(apply(post[,-c(1:5)], 1, mean)), 1)
+# q = round(quantile(apply(post[,-c(1:5)], 1, mean), probs=c(0.025, 0.05, 0.5, 0.975)), 1)
+# gg + geom_line(aes(x = Year, y = median), linetype = "dashed") + 
+#   geom_text(aes(x = 2003, y = 3000, label = "20-year mean", hjust = 0)) + 
+#   geom_text(aes(x = 2003, y = 2800, hjust = 0,
+#                 label = paste0(m, " (95% CI: ", q[1], ", ", q[4], ")")))
+# ggsave("results/combined_year_detection_High.png")
